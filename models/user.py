d6ece93c8b70de5bf31b465e95c9fade64702d04
kyh-66 <3493598861@qq.com>
2026-03-16 00:06:32 +0800

    加了数据库模型

--- models/user.py
+++ models/user.py
@@ -1,11 +1,45 @@
 from . import Base
 from sqlalchemy.orm import mapped_column, Mapped
-from sqlalchemy import Integer, String
+from sqlalchemy import Integer, String, DateTime
+from pwdlib import PasswordHash
+from datetime import datetime
 
+password_hash = PasswordHash.recommended()
 
 class User(Base):
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
     username: Mapped[str] = mapped_column(String(100))
-    password: Mapped[str] = mapped_column(String(100))
+    _password: Mapped[str] = mapped_column(String(100))
+
+
+    def __int__(self, *args, **kwargs):
+        password = kwargs.pop("password")
+        if password:
+            self._password = password_hash.verify(password)
+            super().__int__(*args, **kwargs)
+            if password:
+                self.password = password
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, raw_password):
+       self._password = password_hash.hash(raw_password)
+
+#验证密码
+    def check_password(self, raw_password):
+        return password_hash.verify(raw_password, self.password)
+
+
+
+    #邮箱
+    class EmailCode(Base):
+        __tablename__ = "email_code"
+        id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+        email: Mapped[str] = mapped_column(String(100))
+        code: Mapped[str] = mapped_column(String(10))
+        #创建时间
+        created_time: Mapped[datetime] = mapped_column(DateTime)
